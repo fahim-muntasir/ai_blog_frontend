61bdeef8c03dd0d95ee94ae7486efe40ca8f276d
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-11-15 20:10:05 +0600

    fix: resolve responsive issue for large screens (lg)

--- components/ResourceSection/ResourceCard.tsx
+++ components/ResourceSection/ResourceCard.tsx
@@ -12,17 +12,17 @@ export default function ResourceCard({
       <div className="container mx-auto px-6 md:px-0 md:flex">
         <div className="w-full md:w-1/3 py-14 border-b md:border-b-0 md:border-r md:pr-16 border-dark20">
           <Image
-            className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] mb-4 md:mb-8"
+            className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] mb-4 md:mb-8"
             src={resourceCard.icon}
             alt={resourceCard.icon}
             width={50}
             height={50}
             quality={100}
           />
-          <h3 className=" font-kumbh text-2xl md:text-3xl mb-2 font-semibold ">
+          <h3 className=" font-kumbh text-2xl md:text-3xl lg:text-[40px] mb-2 font-semibold ">
             {resourceCard.title}
           </h3>
-          <p className="text-sm text-grey60 mb-4 md:mb-8 md:text-base">
+          <p className="text-sm text-grey60 mb-4 md:mb-8 md:text-base lg:text-xl">
             {resourceCard.description}
           </p>
           <Button
@@ -33,19 +33,19 @@ export default function ResourceCard({
           <div className="mt-4 md:mt-8">
             <div className="bg-secondary flex p-8 md:p-6 rounded-md border border-dark20 justify-between">
               <div className="flex flex-col justify-center">
-                <span className="text-sm text-grey60">
+                <span className="text-sm lg:text-lg text-grey60">
                   {resourceCard.downloadInfo.downloadedBy.label}
                 </span>
-                <span className="font-semibold text-lg">
+                <span className="font-semibold text-lg lg:text-2xl">
                   {resourceCard.downloadInfo.downloadedBy.count}
                 </span>
               </div>
 
               <div className="flex bg-dark8 items-center w-[144px] h-[56px] md:w-[149px] md:h-[56px] rounded-xl justify-center">
                 {resourceCard.downloadInfo.avatars.map((avatar, index) => (
-                  <div key={index} className="relative ml-0">
+                  <div key={index} className={`relative ml-[${index !== 0 ? "-10px" : "0px"}]`}>
                     <Image
-                      className="rounded-full w-[40px] h-[40px] md:w-[40px] md:h-[40px] border-2 border-dark40"
+                      className="rounded-full w-[40px] h-[40px] lg:w-[40px] lg:h-[40px] border-2 border-dark40"
                       src={avatar.src}
                       alt={avatar.alt}
                       width={60}
--- components/ResourceSection/ResourceTopic.tsx
+++ components/ResourceSection/ResourceTopic.tsx
@@ -11,15 +11,15 @@ export default function ResourceTopic({
   return (
     <div className="py-14 w-full md:w-2/3 md:pl-16">
       <div className="block md:flex justify-between gap-2">
-        <h3 className="text-lg font-semibold md:text-xl mb-4 md:w-80">
+        <h3 className="text-lg font-semibold md:text-xl lg:text-2xl mb-4 md:w-80">
           {title}
         </h3>
-        <p className="text-sm text-grey60 mb-6 md:text-base">
+        <p className="text-sm text-grey60 mb-6 md:text-base lg:text-lg">
           {topicsDescription}
         </p>
       </div>
       <Image
-        className="w-full h-[169px] md:h-[275px] rounded-xl"
+        className="w-full h-[169px] md:h-[275px] lg:h-[332px] rounded-xl"
         src={avatar.src}
         alt={avatar.alt}
         width={500}
